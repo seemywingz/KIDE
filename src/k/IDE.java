05028b83011a0fe306e04602fb256fc453528641
seemywingz <gamedev@kevservices.com>
2015-01-23 23:11:35 -0500

    Initial upload

---
+++ src/k/IDE.java
@@ -0,0 +1,31 @@
+package k;
+
+import javax.swing.*;
+
+/**
+ * Created by kevin on 1/23/15.
+ */
+
+public class IDE extends JFrame{
+
+
+
+    IDE(){
+
+        setSize(800,600);
+        setTitle("KIDE");
+
+
+
+
+
+
+
+        setVisible(true);
+    }//..
+
+    public static void main(String[] args) {
+        new IDE();
+    }//..
+
+}// IDE
