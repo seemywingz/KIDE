18832b6d7e83c87d42ef7c5abe0425f4ab2fb139
seemywingz <gamedev@kevservices.com>
2015-02-12 15:33:43 -0800

    Proper Token Types for class

--- src/k/Parser.java
+++ src/k/Parser.java
@@ -10,10 +10,6 @@ import java.util.ArrayList;
 public class Parser extends ScrollableOutput {
 
     protected String parseErrors, errorPrefix="\nParse Error on line ";
-    private ArrayList<Token> tokensInGlobalScope = new ArrayList<Token>();
-    private ArrayList<Token> tokensInLocalScope = new ArrayList<Token>();
-    private ArrayList<String> definedVariables = new ArrayList<String>();
-    private boolean inNewScope;
 
     Parser(IDEPanel idePanel1) {
         super(idePanel1);
@@ -25,69 +21,14 @@ public class Parser extends ScrollableOutput {
         initScrollPane(new Rectangle(idePanel1.lex.getScrollPane().getX()+idePanel1.lex.getScrollPane().getWidth(),2,w,h));
     }//..
 
-    public void parse(ArrayList<Token> tokens){
-        parseErrors = "";
-        textArea.setText(title);
-        tokensInGlobalScope = new ArrayList<Token>();
-        tokensInLocalScope = new ArrayList<Token>();
-        definedVariables = new ArrayList<String>();
 
-//        if(tokens.get(0).getType() != TokenType.BEGIN){
-//            idePanel.editor.addErrorLineNumber(tokens.get(0).getLineNum());
-//            parseErrors+="\nParse Error on line "+(tokens.get(0).getLineNum()+1)+": program must start with 'begin:'";
-//        }
+   private void parseProgram(){
 
-        for(int i=0;i<tokens.size();i++){
-                // Recursive Descent Parser
-                Token token = tokens.get(i);
-                int tokensChecked = 0;
-                switch (token.getType()) {
-                    case TYPE:
-                        i += typeCheck(tokens, i);
-                        break;
 
-                    default:
-                }
-        }//
+   }//..
 
-//        if(tokens.get(tokens.size()-1).getType()!= TokenType.END){
-//            idePanel.editor.addErrorLineNumber(tokens.get(tokens.size()-1).getLineNum());
-//            parseErrors+="\nParse Error on line "+(tokens.get(tokens.size()-1).getLineNum()+1)+": program must end with 'end.'";
-//        }
-        idePanel.editor.drawLines();
-        textArea.append(parseErrors);
-    }//..
+    private void parseBlock(){
 
-    private int typeCheck(ArrayList<Token> tokens,int i){
-        int tokensParsed = 0;
-        Token token;
-        try {
-            if ((token = tokens.get(i + 1)).getType() == TokenType.ID) {
-                String data = token.getData();
-                if(inNewScope){
-
-                }else{
-                    boolean err=false;
-                    for (Token t:tokensInGlobalScope){
-                        if(t.getData().equals(data)) {
-                            parseErrors += errorPrefix +( token.getLineNum()+1) + ": variable '" + data + "' already defined in this scope";
-                            err=true;
-                            idePanel.editor.addErrorLineNumber(token.getLineNum());
-                        }
-                    }
-                    if(!err){
-                        tokensInGlobalScope.add(token);
-                    }
-                }
-
-            } else {
-                addParseError(tokens.get(i + 1).getLineNum(), TokenType.ID, tokens.get(i + 1).getType());
-                return 1;
-            }
-            return tokensParsed;
-        }catch (Exception e){
-            return tokensParsed;
-        }
     }//..
 
     protected void addParseError(int line,TokenType expected,TokenType found){
